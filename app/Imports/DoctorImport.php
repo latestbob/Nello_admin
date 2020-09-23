572ae4921b1cd8844c4e52a234cafc1e428ab8c7
Olu Udeh <oluudeh@gmail.com>
2020-09-23 09:26:33 -0700

    doctor import

---
+++ app/Imports/DoctorImport.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class DoctorImport implements ToModel, WithHeadingRow
+{
+    
+    public function headingRow() : int 
+    {
+        return 1;
+    }
+
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        $names = explode(' ', $row['medical_director']);
+        $count = count($names);
+        return new User([
+            'uuid'      => Str::uuid()->toString(),
+            'firstname' => $names[1],
+            'middlename' => $count > 3 ? $names[2] : '',
+            'lastname' => $count > 3 ? $names[3] : ($count == 3 ? $names[2] : '') ,
+            'phone' => $row['tel_no'],
+            'email' => $row['email_address'],
+            'password' => Hash::make(Str::random(8)),
+            'picture' => '',
+            //'ufield' => '',
+            'aos' => $row['specialization'],
+            'gender' => '',
+            'user_type' => 'doctor',
+            'vendor_id' => 1        
+        ]);
+    }
+}
--- app/Imports/DrugsImport.php
+++ app/Imports/DrugsImport.php
@@ -3,10 +3,7 @@
 namespace App\Imports;
 
 use Illuminate\Support\Str;
-use Illuminate\Support\Collection;
-use App\Models\PharmacyDrug as Drug;
 use App\Models\PharmacyDrug;
-use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
@@ -37,27 +34,4 @@ class DrugsImport implements ToModel, WithHeadingRow
         }
     }
 
-    public function collection(Collection $rows)
-    {
-        $drugs = [];
-        foreach ($rows as $row)
-        {
-            if (!empty(trim($row[0])) && is_numeric($row[3])) {
-                $drugs[] = [
-                    'sku' => $row[0],
-                    'name' => $row[1],
-                    'brand' => $row[2],
-                    'category_id' => $row[3],
-                    'dosage_type' => $row[4],
-                    'price' => (double) str_replace(',', '', $row[5]),
-                    'require_prescription' => strtolower($row[6]) == 'no' ? 0 : 1,
-                    'description' => $row[7],
-                    'vendor_id' => 1,
-                    'uuid' => Str::uuid()->toString()
-                ];
-            }
-        }
-
-        Drug::insert($drugs);
-    }
 }
