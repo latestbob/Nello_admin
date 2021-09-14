9735e7737319faff309ca568bc5772c6e74aa070
Olu Udeh <oluudeh@gmail.com>
2021-09-14 05:36:32 -0700

    drugs import

--- app/Imports/DrugsImport.php
+++ app/Imports/DrugsImport.php
@@ -21,31 +21,31 @@ class DrugsImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         if (!is_numeric($row['category']) && !empty($row['category'])) {
-            $this->count++;
-            print_r($row);
-            echo "\n{$this->count} \n";
-            //     $category = DrugCategory::where('name', $row['category'])->first();
-            // if (!$category) {
-            //     $category = DrugCategory::create([
-            //         'name' => $row['category']
-            //     ]);
-            // }
+            // $this->count++;
+            // print_r($row);
+            // echo "\n{$this->count} \n";
+            $category = DrugCategory::where('name', $row['category'])->first();
+            if (!$category) {
+                $category = DrugCategory::create([
+                    'name' => $row['category']
+                ]);
+            }
 
-            // if (isset($row['drug_id'])) {
-            //     return new PharmacyDrug([
-            //         'sku' => $row['drug_id'],
-            //         'name' => $row['item_name'],
-            //         'brand' => $row['brand'],
-            //         'quantity' => $row['quantity'],
-            //         'category_id' => $category->id,
-            //         'dosage_type' => $row['dosage_form'],
-            //         'price' => (float) str_replace(',', '', $row['price']),
-            //         'require_prescription' => strtolower($row['require_prescription']) == 'no' ? 0 : 1,
-            //         'description' => $row['description'],
-            //         'vendor_id' => 1,
-            //         'uuid' => Str::uuid()->toString()
-            //     ]);
-            // }
+            if (isset($row['drug_id'])) {
+                return new PharmacyDrug([
+                    'sku' => $row['drug_id'],
+                    'name' => $row['item_name'],
+                    'brand' => $row['brand'],
+                    'quantity' => $row['quantity'],
+                    'category_id' => $category->id,
+                    'dosage_type' => $row['dosage_form'],
+                    'price' => (float) str_replace(',', '', $row['price']),
+                    'require_prescription' => strtolower($row['require_prescription']) == 'no' ? 0 : 1,
+                    'description' => $row['description'],
+                    'vendor_id' => 1,
+                    'uuid' => Str::uuid()->toString()
+                ]);
+            }
         }
     }
 }
