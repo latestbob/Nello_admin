af5709c61e107924ea711360a06768e62a0403ae
Olu Udeh <oluudeh@gmail.com>
2021-09-14 05:40:19 -0700

    drugs import

--- app/Imports/DrugsImport.php
+++ app/Imports/DrugsImport.php
@@ -31,7 +31,7 @@ class DrugsImport implements ToModel, WithHeadingRow
                 ]);
             }
 
-            if (isset($row['drug_id'])) {
+            if (isset($row['drug_id']) && !empty($row['name'])) {
                 return new PharmacyDrug([
                     'sku' => $row['drug_id'],
                     'name' => $row['item_name'],
