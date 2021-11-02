e438a894b464a5b9e3736c774397f45272ce656d
Ikay <oluudeh@gmail.com>
2021-11-02 15:10:36 +0100

    partners

---
+++ app/Http/Controllers/PartnersController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PartnerRequest;
+use App\Models\Partner;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PartnersController extends Controller
+{
+    public function index(Request $request)
+    {
+        $partners = Partner::paginate($request->size ?? 15);
+
+        return $partners;
+    }
+
+
+    public function create(PartnerRequest $request)
+    {
+        $data = $request->validated();
+        $data['api_key'] = Str::lower(Str::random(16));
+        Partner::create($data);
+    }
+
+
+}
