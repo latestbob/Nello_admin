e589dd1162c2da5a3624941bc556e6ec40a839cd
unknown <ogidomoses@gmail.com>
2022-08-24 09:57:03 +0100

    Embanqo apicompleted

--- app/Http/Controllers/Otpcontroller.php
+++ app/Http/Controllers/Otpcontroller.php
@@ -5,27 +5,52 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Otp;
 
-
 class Otpcontroller extends Controller
 {
     //
 
-    public function generateotp(){
+    public function generateotp(Request $request){
+
+$validator = Validator::make($request->all(), [
+            'phone' => 'required|string'
+        ]);
+
+
+if ($validator->fails()) {
+            return response([
+                'status' => 'failed',
+                'message' => $validator->errors()
+            ]);
+        }
+
         $otp = new Otp;
-    $otp_token = $otp->generate('nello', 6, 3);
+    $otp_token = $otp->generate($request->phone, 6, 10);
     
     return response()->json($otp_token);
     }
 
+
     //validate otp
 
     public function validateotp(Request $request){
+        $validator = Validator::make($request->all(), [
+            'phone' => 'required|string',
+                'code' => 'required|string'
+        ]);
+
+
+if ($validator->fails()) {
+            return response([
+                'status' => 'failed',
+                'message' => $validator->errors()
+            ]);
+        }
+
         $code = $request->code;
         $otp = new Otp;
-    $valid = $otp->validate('nello', $code);
+    $valid = $otp->validate($request->phone, $code);
     
     return response()->json($valid);
     }
-
 
 }
